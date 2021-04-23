72e6674d091bdab04b34443176047f1764eaf5ad
Robinson Junior <robinson.juniorr@gmail.com>
2021-04-22 23:37:54 -0300

    configurando react query dev tools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -24,23 +24,29 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
-    return users;
-  });
+  const { data, isLoading, error } = useQuery(
+    "users",
+    async () => {
+      const response = await fetch("http://localhost:3000/api/users");
+      const data = await response.json();
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+        };
+      });
+      return users;
+    },
+    {
+      staleTime: 1000 * 5, //5 segundos
+    }
+  );
   // console.log(query);
   const isWideVersion = useBreakpointValue({ base: false, lg: true });
 
